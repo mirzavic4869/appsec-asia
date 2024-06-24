c1d52a22969f31d5563c99b155fcfee847b7c255
Mirzam Avicena <avicenamirzam@gmail.com>
2024-06-24 10:09:35 +0700

    remove our clients section

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <TestimonySection />
       <MotoSection />
       <TrainingsSection />
-      <OurClientSection />
+      {/* <OurClientSection /> */}
       <ContactUsSection />
     </>
   )
